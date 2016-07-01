0a9e0155c80db75d29eeaf77df0a08f72d14d565
Claudio Pascual <claudio.pascual@gmail.com>
2016-07-01 13:22:28 +0100

    Environment prod/dev/test support by env variable

--- web/index.php
+++ web/index.php
@@ -1,8 +1,10 @@
 <?php
 
 // comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
+if (isset($_SERVER['YII_ENV'])) define('YII_ENV', $_SERVER['YII_ENV']);
 defined('YII_ENV') or define('YII_ENV', 'dev');
+
+if (YII_ENV_DEV) defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
