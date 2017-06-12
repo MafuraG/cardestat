4dab72157d565d8f925e28f5e830a82423338d57
Claudio Pascual <claudio.pascual@gmail.com>
2017-06-12 12:57:45 +0100

    Add secure db.php config file

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+
+//$dbopts = parse_url($_SERVER['DATABASE_URL']);
+$dbopts = parse_url(getenv('DATABASE_URL'));
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:dbname='.ltrim($dbopts['path'],'/').';host='.$dbopts['host'].';port='.$dbopts['port'],
+    'username' => $dbopts['user'],
+    'password' => $dbopts['pass'],
+    'charset' => 'utf8',
+];
